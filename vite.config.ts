60fe891155d63d7ee17fb93904831b0ef59ab956
McPreacher <pastorneal84@gmail.com>
2025-10-27 22:12:56 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// IMPORTANT: if your GitHub repo name is NOT 'hide-his-word',
-// change base to `"/<your-repo-name>/"` before deploying.
-export default defineConfig({
-  plugins: [react()],
-  base: "/HideHisWord/",
-});
