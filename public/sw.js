2da5b9f797794813421af131e562b3cd330848ba
McPreacher <pastorneal84@gmail.com>
2025-10-27 22:10:35 -0400

    Delete public directory

--- public/sw.js
+++
@@ -1,4 +0,0 @@
-// Minimal service worker for installability
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
-// App uses localStorage; fetches go straight to network by default.
